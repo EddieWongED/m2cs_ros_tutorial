7ab9738700fe587de8426c433f693423f6426100
EddieED <aolaeddie@gmail.com>
2021-09-29 20:44:12 +0800

    Finished assignment 2 orientation part

--- turtle_path/src/path_manager.py
+++ turtle_path/src/path_manager.py
@@ -2,13 +2,16 @@
 import rospy
 from math import pi, fmod, sin, cos, sqrt
 from geometry_msgs.msg import Twist
-# hint: some imports are missing
+from turtlesim.msg import Pose
+from turtle_path.srv import *
 
 cur_pos = Pose()
 
-def cb_pose(data): # get the current position from subscribing the turtle position
+
+def cb_pose(data):
     global cur_pos
     cur_pos = data
+
 
 def cb_walk(req):
     if (req.distance < 0):
@@ -17,48 +20,78 @@ def cb_walk(req):
     # hint: calculate the projected (x, y) after walking the distance,
     # and return false if it is outside the boundary
 
-    rate = rospy.Rate(100) # 100Hz control loop
+    rate = rospy.Rate(100)  # 100Hz control loop
 
-    while ('''goal not reached'''): # control loop
-        
+    while ('''goal not reached'''):  # control loop
+
         # in each iteration of the control loop, publish a velocity
 
         # hint: you need to use the formula for distance between two points
-        
+
         rate.sleep()
-    
-    vel = Twist() # publish a velocity 0 at the end, to ensure the turtle really stops
+
+    vel = Twist()  # publish a velocity 0 at the end, to ensure the turtle really stops
     pub.publish(vel)
 
     return True
+
 
 def cb_orientation(req):
+    rate = rospy.Rate(100)
 
-    rate = rospy.Rate(100) # 100Hz control loop
-    
-    while ('''goal not reached'''): # control loop
-        
-        # in each iteration of the control loop, publish a velocity
+    rospy.loginfo(cur_pos.theta)
+    rospy.loginfo(req.theta)
 
-        # hint: signed smallest distance between two angles: 
-        # see https://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
-        #     dist = fmod(req.orientation - cur_pos.theta + pi + 2 * pi, 2 * pi) - pi
-        
-        rate.sleep()
-    
-    vel = Twist() # publish a velocity 0 at the end, to ensure the turtle really stops
-    pub.publish(vel)
+    theta_diff = req.theta - cur_pos.theta
+    if (theta_diff > pi):
+        theta_diff -= 2 * pi
+    elif (theta_diff < -pi):
+        theta_diff += 2 * pi
 
-    return True
+    if (theta_diff > 2 * pi or theta_diff < -2 * pi):
+        rospy.loginfo("Input theta is too large (range: -2PI to 2PI)")
+        resp = SetOrientationResponse()
+        resp.success = False
+        return resp
+    else:
+        while (theta_diff != 0):  # control loop
+
+            twist = Twist()
+            twist.angular.z = theta_diff * 10
+            pub.publish(twist)
+
+            theta_diff = req.theta - cur_pos.theta
+            if (theta_diff > pi):
+                theta_diff -= 2 * pi
+            elif (theta_diff < -pi):
+                theta_diff += 2 * pi
+
+            rate.sleep()
+
+    rospy.loginfo("Successfully set orientation")
+    twist = Twist()
+    pub.publish(twist)
+
+    resp = SetOrientationResponse()
+    resp.success = True
+    return resp
+
 
 if __name__ == '__main__':
     rospy.init_node('path_manager')
-    
-    pub = # publisher of the turtle velocity
-    sub = # subscriber of the turtle position, callback to cb_pose
-    
-    ## init each service server here:
+
+    # Subscribe to /turtle1/pose
+    rospy.Subscriber("/turtle1/pose", Pose, cb_pose)
+
+    # Hosting service /set_orientation
+    rospy.Service('set_orientation', SetOrientation, cb_orientation)
+    rospy.loginfo("Service /set_orientation is ready")
+
+    # Publishing velocity to /turtle1/cmd_vel
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=1)
+
+    # init each service server here:
     # rospy.Service( ... )		# callback to cb_orientation
     # rospy.Service( ... )		# callback to cb_walk
-    
+
     rospy.spin()
